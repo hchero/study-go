6674a05d169ac89190e6d249e81254184ac25131
super <zhou4xin3xin3520@163.com>
2022-11-09 11:34:00 +0800

    结构体的定义

---
+++ day03/struct/define/main.go
@@ -0,0 +1,28 @@
+/*
+ * @Author: super zhou4xin3xin3520@163.com
+ * @Date: 2022-11-09 11:27:56
+ * @LastEditors: super
+ * @LastEditTime: 2022-11-09 11:33:06
+ * @Description:结构体的定义
+ */
+package main
+
+import "fmt"
+
+type Person struct {
+	Name   string
+	Age    int
+	Scores [5]float64        // 数组
+	ptr    *int              // 指针
+	slice  []int             // 切片
+	map1   map[string]string // map
+}
+
+func main() {
+	var p1 Person
+
+	p1.ptr = &p1.Age
+	p1.slice = make([]int, 10)
+	p1.map1 = make(map[string]string, 10)
+	fmt.Println(p1)
+}
